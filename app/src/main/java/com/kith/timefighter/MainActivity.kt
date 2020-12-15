471e8783a8aa541d41d57ca3ddc892e1dcd77e81
Tumi <oluwatumininuojo25@gmail.com>
2020-12-15 06:40:55 +0100

    A simple game where you have to race against time and make as many clicks as possible

---
+++ app/src/main/java/com/kith/timefighter/MainActivity.kt
@@ -0,0 +1,164 @@
+package com.kith.timefighter
+
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.animation.AnimationUtils
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.kith.timefighter.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    // Initialisation of relevant properties
+    lateinit var binding: ActivityMainBinding
+    private var score = 0
+    private var gameStarted = false
+    private lateinit var countDownTimer: CountDownTimer
+    private var initialCountDown: Long = 60000
+    private var countDownInterval: Long = 1000
+    private var timeLeft = 60
+
+
+    // Companion object to store relevant states when the activity is destroyed
+    companion object {
+        private const val SCORE_KEY = "SCORE_KEY"
+        private const val TIME_LEFT_KEY = "TIME_LEFT_KEY"
+    }
+
+    // Initialisation for logging
+    private val TAG = MainActivity::class.java.simpleName
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setContentView(binding.root)
+        Log.d(TAG, "onCreate called, Score is: $score")
+
+        if (savedInstanceState != null) {
+            score = savedInstanceState.getInt(SCORE_KEY)
+            timeLeft = savedInstanceState.getInt(TIME_LEFT_KEY)
+            restoreGame()
+        } else {
+            resetGame()
+        }
+
+        binding.tapMeButton.setOnClickListener {
+            incrementScore()
+        }
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putInt(SCORE_KEY, score)
+        outState.putInt(TIME_LEFT_KEY, timeLeft)
+        countDownTimer.cancel()
+
+        Log.d(TAG, "onSaveInstanceState: Saving score: $score & Time left: $timeLeft")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.d(TAG, "onDestroy called")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.about_item) {
+            showInfo()
+        }
+        return true
+    }
+
+    private fun incrementScore() {
+        if (!gameStarted) {
+            startGame()
+        }
+        score++
+        val newScore = getString(R.string.your_score_1_d, score)
+        binding.yourScoreTextView.text = newScore
+    }
+
+    private fun resetGame() {
+        score = 0
+
+        val initialScore = getString(R.string.your_score_1_d, score)
+        binding.yourScoreTextView.text = initialScore
+
+        val initialTimeLeft = getString(R.string.time_left_1_d, 60)
+        binding.timeLeftTextView.text = initialTimeLeft
+
+        countDownTimer = object : CountDownTimer(initialCountDown, countDownInterval) {
+            override fun onFinish() {
+                endGame()
+            }
+
+            override fun onTick(millisUntilFinished: Long) {
+                timeLeft = millisUntilFinished.toInt() / 1000
+
+                val timeLeftString = getString(R.string.time_left_1_d, timeLeft)
+                binding.timeLeftTextView.text = timeLeftString
+            }
+        }
+
+        gameStarted = false
+    }
+
+    private fun startGame() {
+        countDownTimer.start()
+        gameStarted = true
+    }
+
+    private fun endGame() {
+        Toast.makeText(this, getString(R.string.game_over, score), Toast.LENGTH_SHORT).show()
+        resetGame()
+    }
+
+    private fun restoreGame() {
+        val restoredScore = getString(R.string.your_score_1_d, score)
+        binding.yourScoreTextView.text = restoredScore
+
+        val restoredTime = getString(R.string.time_left_1_d, timeLeft)
+        binding.timeLeftTextView.text = restoredTime
+
+        countDownTimer = object : CountDownTimer((timeLeft * 1000).toLong(), countDownInterval) {
+            override fun onFinish() {
+                endGame()
+            }
+
+            override fun onTick(millisUntilFinished: Long) {
+                timeLeft = millisUntilFinished.toInt() / 1000
+
+                val timeLeftString = getString(R.string.time_left_1_d, timeLeft)
+                binding.timeLeftTextView.text = timeLeftString
+            }
+        }
+        countDownTimer.start()
+        gameStarted = true
+    }
+
+    private fun showInfo() {
+        val dialogTitle = getString(R.string.about_title, BuildConfig.VERSION_NAME)
+        val dialogMessage = getString(R.string.about_message)
+
+        val builder = MaterialAlertDialogBuilder(this)
+        builder.setTitle(dialogTitle)
+        builder.setMessage(dialogMessage)
+        builder.create().show()
+    }
+}
+
