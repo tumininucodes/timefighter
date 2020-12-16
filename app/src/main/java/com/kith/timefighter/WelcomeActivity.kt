decfdea35d48c0ec8d8a90b08db4f81413ab178b
Tumi <oluwatumininuojo25@gmail.com>
2020-12-16 12:40:31 +0100

    A simple game where you have to race against time and make as many clicks as possible

---
+++ app/src/main/java/com/kith/timefighter/WelcomeActivity.kt
@@ -0,0 +1,16 @@
+package com.kith.timefighter
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WelcomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+
+
+    }
+}
